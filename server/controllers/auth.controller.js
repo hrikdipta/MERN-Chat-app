91a449ab0d6f78f45e5f567324438179cd05e446
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-05-25 11:22:11 +0530

    created login api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const signup=async(req,res,next)=>{
     try{
         const {name,email,password}=req.body;
@@ -15,6 +16,44 @@ export const signup=async(req,res,next)=>{
         const newUser=await User.create({name,email,password:hashedPassword});
         const {password:pass,...rest}=newUser._doc;
         return res.status(201).json(rest);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const checkEmail=async(req,res,next)=>{
+    try{
+        const {email}=req.body;
+        if(!email){
+            return next(errorHandler(400,"Email is required"))
+        }
+        const user=await User.findOne({email});
+        if(!user){
+            return next(errorHandler(400,"User doesn't exists"))
+        }
+        return res.status(200).json({message:"User is available"})
+    }   catch(err){
+        next(err);
+    }
+}
+
+export const login =async(req,res,next)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return next(errorHandler(400,"All fields are required"))
+        }
+        const user=await User.findOne({email});
+        if(!user){
+            return next(errorHandler(400,"User doesn't exists"))
+        }
+        const isMatch=await bcryptjs.compare(password,user.password);
+        if(!isMatch){
+            return next(errorHandler(400,"Invalid credentials"))
+        }
+        const token=jwt.sign({id:user._id,email:user.email},process.env.JWT_SECRET);
+        const {password:pass,...rest}=user._doc;
+        return res.cookie('token',token).status(200).json(rest);
     }catch(err){
         next(err);
     }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup,checkEmail ,login} from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/signup', signup);
-
+router.post('/checkemail',checkEmail);
+router.post('/login',login);
 export default router;
