a4f819016394c58585f7017d5ca8cea01dc1dbea
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-05-26 18:21:41 +0530

    added functionality to register page

--- client/src/components/RegisterComponent.jsx
+++ client/src/components/RegisterComponent.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Card,
     Input,
     Button,
     Typography,
+    Spinner
   } from "@material-tailwind/react";
+  import {useNavigate} from 'react-router-dom';
 const RegisterComponent = () => {
+    const navigate=useNavigate();
+    const [formData,setFormData]=useState({name:'',email:'',password:''});
+    const [error,setError]=useState(null);
+    const [loading,setLoading]=useState(false);
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        if(!formData.name||!formData.email||!formData.password){
+            setError('Please fill all the fields');
+            return;
+        }
+        try {
+            setLoading(true);
+            setError(null);
+            const res=await fetch('/api/auth/signup',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(formData)
+            });
+            const data=await res.json();
+            if(!res.ok){
+                setError(data.message);
+                setLoading(false);
+                return;
+            }
+            setLoading(false);
+            navigate('/chat');
+        } catch (error) {
+            setError('Something went wrong');
+            setLoading(false);
+        }
+    }
   return (
     <div>
       <Card color="transparent" shadow={false} className='flex flex-col justify-center items-center'>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
         <Typography variant="h4" color="blue-gray">
             Register
         </Typography>
@@ -28,6 +63,8 @@ const RegisterComponent = () => {
                 labelProps={{
                 className: "before:content-none after:content-none",
                 }}
+                name='name'
+                onChange={(e)=>{setFormData({...formData,name:e.target.value})}}
             />
           <Typography variant="h6" color="blue-gray" className="-mb-3">
             Your Email
@@ -39,6 +76,8 @@ const RegisterComponent = () => {
             labelProps={{
               className: "before:content-none after:content-none",
             }}
+            name='email'
+            onChange={(e)=>{setFormData({...formData,email:e.target.value})}}
           />
           <Typography variant="h6" color="blue-gray" className="-mb-3">
             Password
@@ -51,12 +90,19 @@ const RegisterComponent = () => {
             labelProps={{
               className: "before:content-none after:content-none",
             }}
+            name='password'
+            onChange={(e)=>{setFormData({...formData,password:e.target.value})}}
           />
         </div>
-        <Button className="mt-6" fullWidth>
-          Register
+        <Button type='submit' className="mt-6 text-center flex justify-center" fullWidth disabled={loading} >
+          {
+                loading?<Spinner />:'Register'
+          }
         </Button>
       </form>
+      {
+            error&&<Typography color='red'>{error}</Typography>
+      }
     </Card>
     </div>
   )
