66d6283057a7dc7fecd9ec6808c353841ce027f6
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-05-27 16:05:27 +0530

    updated login functionality

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -1,14 +1,50 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import {
   Card,
   Input,
   Button,
   Typography,
+  Spinner
 } from "@material-tailwind/react";
+import {useNavigate} from 'react-router-dom';
 const LoginComponent = () => {
+  const navigate=useNavigate();
+  const [formData,setFormData]=useState({email:'',password:''});
+  const [error,setError]=useState(null);
+  const [loading,setLoading]=useState(false);
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    if(!formData.email||!formData.password){
+      setError('Please fill all the fields');
+      return;
+    }
+    try {
+      setLoading(true);
+      setError(null);
+      const res=await fetch('/api/auth/login',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formData)
+      });
+      const data=await res.json();
+      if(!res.ok){
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      localStorage.setItem('user',JSON.stringify(data));
+      navigate('/chat');
+    } catch (error) {
+      setError('Something went wrong');
+      setLoading(false);
+    }
+  }
   return (
     <Card color="transparent" shadow={false} className='flex flex-col justify-center items-center'>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
         <Typography variant="h4" color="blue-gray">
           Log in
         </Typography>
@@ -26,6 +62,8 @@ const LoginComponent = () => {
             labelProps={{
               className: "before:content-none after:content-none",
             }}
+            name='email'
+            onChange={(e)=>setFormData({...formData,email:e.target.value})}
           />
           <Typography variant="h6" color="blue-gray" className="-mb-3">
             Password
@@ -38,13 +76,20 @@ const LoginComponent = () => {
             labelProps={{
               className: "before:content-none after:content-none",
             }}
+            name='password'
+            onChange={(e)=>setFormData({...formData,password:e.target.value})}
           />
         </div>
         
-        <Button className="mt-6" fullWidth>
-          Log in
+        <Button type='submit' className="mt-6 text-center flex justify-center" fullWidth>
+          {
+            loading?<Spinner/>:'Log in'
+          }
         </Button>
       </form>
+      {
+            error&&<Typography color='red'>{error}</Typography>
+      }
     </Card>
   )
 }
--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -11,7 +11,7 @@ import LoginComponent from '../components/LoginComponent';
 import RegisterComponent from '../components/RegisterComponent';
 const AuthPage = () => {
   return (
-    <div className=' max-w-2xl flex flex-col mx-auto'>
+    <div className=' max-w-2xl flex flex-col mx-auto h-screen my-14'>
       <Tabs value="login">
         <TabsHeader>
           <Tab value="login">Login</Tab>
